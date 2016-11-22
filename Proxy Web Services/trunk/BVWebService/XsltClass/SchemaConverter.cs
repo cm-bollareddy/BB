6551e97ff7c874d1158ff36ad1e57d87a510a313
brboggio <brboggio@d7a1bf34-9008-0343-ae66-88e8bea8f563>
2016-11-22 03:01:52 +0000

    Logging changes to improve readability of resulting logs.
    git-svn-id: https://svn.hq.corp.pbs.org:8443/svn/PBS/Active%20Projects/BroadView@23351 d7a1bf34-9008-0343-ae66-88e8bea8f563

--- Proxy Web Services/trunk/BVWebService/XsltClass/SchemaConverter.cs
+++ Proxy Web Services/trunk/BVWebService/XsltClass/SchemaConverter.cs
@@ -14,46 +14,54 @@ namespace BVWebService
 	{
 		public SchemaConverter()
 		{
-			//
-			// TODO: Add constructor logic here
-			//
 		}
 
+
+        /// <summary>
+        /// Take the Dataset from the caller and export it to XML (Diffgram).
+        /// Load the XML (Diffgram) into an XML Document.
+        /// Load up the appropriate XSLT into an XML document to transform Dataset XML into Broadview XML.
+        /// Transform the source DataSet XML into BroadView XML
+        /// Return the transformed BroadView XML result to the caller.        
+        /// </summary>
+        /// <param name="dataSet"></param>
+        /// <param name="xsltFile"></param>
+        /// <returns></returns>
 		public static string ToXml(DataSet dataSet, string xsltFile)
 		{
-			//
 			// Trace the input data
-			//
 			Tracer oTracer = new Tracer();
-            if (oTracer.IsDebugEnabled)	// Check the switch before tracing (we don't want to extract the XML if the switch is OFF)...
+
+            if (oTracer.IsDebugEnabled)	
 			{
-				StringWriter oStringWriter = new StringWriter();
-				dataSet.WriteXml(oStringWriter, XmlWriteMode.WriteSchema | XmlWriteMode.DiffGram);
-				oTracer.LogInfo("Entering SchemaConverter.ToXml.\nDataSet=\n" + oStringWriter.ToString());
+                oTracer.LogDebug(string.Format("In SchemaConverter.ToXml().\nSource DataSet=\n{0}\nSource XSLT=\n{1}\n", dataSet.GetXml(), xsltFile));
+
+                //using (StringWriter oStringWriter = new StringWriter())
+                //{
+                //    dataSet.WriteXml(oStringWriter, XmlWriteMode.WriteSchema | XmlWriteMode.DiffGram);
+                //    
+                //}
 			}
 
 			//
 			// We don't want the namespace, so null it out!
 			// (Not sure how to handle the tricky namespace issue... LATER!)
-			//
 			dataSet.Namespace = "";
 
-			// 
 			// Load the DataSet into an XML document (DiffGram approach)
-			//
 			XmlDocument oXmlDataDocument = new XmlDocument();
 			{
-				StringWriter oStringWriter = new StringWriter();
-				dataSet.WriteXml(oStringWriter, XmlWriteMode.DiffGram);
-				oXmlDataDocument.LoadXml(oStringWriter.ToString());
+                using (StringWriter oStringWriter = new StringWriter())
+                {
+                    dataSet.WriteXml(oStringWriter, XmlWriteMode.DiffGram);
+                    oXmlDataDocument.LoadXml(oStringWriter.ToString());
+                }
 			}
 
-			//
 			// Take a look at the XmlDataDocument
-			//
             if (oTracer.IsDebugEnabled)	// Check the switch before tracing (we don't want to extract the XML if the switch is OFF)...
 			{
-				oTracer.LogInfo("Within SchemaConverter.ToXml.\nXmlDataDocument=\n" + oXmlDataDocument.OuterXml);
+                oTracer.LogDebug(string.Format("In SchemaConverter.ToXml().\nSource Dataset DiffGram XmlDocument=\n{0}\n", oXmlDataDocument.OuterXml));
 			}
 
 			//
@@ -62,8 +70,7 @@ namespace BVWebService
             XslCompiledTransform oXsl = new XslCompiledTransform();
 			try
 			{
-
-                //oXsl.Load(xsltFile, XsltSettings.TrustedXslt, new XmlUrlResolver());
+                oTracer.LogDebug(string.Format("In SchemaConverter.ToXml().Loading Source XSLT=\n{0}\n", xsltFile));
                 oXsl.Load(xsltFile);
 			}
 			catch(Exception ex)
@@ -72,9 +79,10 @@ namespace BVWebService
 					ServiceException.ExceptionCode.InvalidXslt,
 					"WebService has failed to load the xslt transformation file: " + xsltFile,
 					ex);
-			}
+            }
 
-			// XmlReader Approach
+            #region ApproachDifferences
+            // XmlReader Approach
 			//				XmlReader oXmlReader = oXsl.Transform(oXmlDataDocument, (XsltArgumentList) null, (XmlResolver) null);
 			//				oXmlReader.MoveToContent();
 			//				string strResult = "<?xml version=\"1.0\" ?>" + oXmlReader.ReadOuterXml());
@@ -89,15 +97,17 @@ namespace BVWebService
 			//				string strResult = oReader.ReadToEnd();
 			//				
 			//				// DEBUG: Save a copy of output
-						
-			//				// DEBUG: Save a copy of output
 
-			// StringWriter Approach
+            //				// DEBUG: Save a copy of output
+            #endregion
+
+            // StringWriter Approach
 			string strResult;
 			using (StringWriter oStringWriter = new StringWriter())
 			{
 				try
 				{
+                    oTracer.LogDebug("In SchemaConverter.ToXml().Transforming XML.");
 					oXsl.Transform(oXmlDataDocument, (XsltArgumentList) null, (TextWriter) oStringWriter);
 				}
 				catch(Exception ex)
@@ -113,24 +123,35 @@ namespace BVWebService
 				
 			}
 
-			
-			oTracer.LogInfo("Leaving SchemaConverter.ToXml.\nstrResult = \n" + strResult);
+		
+
+            if (oTracer.IsDebugEnabled)	
+            {
+                oTracer.LogDebug(string.Format("Leaving SchemaConverter.ToXml().\nTransformed XML Result=\n{0}\n", strResult));
+            }
+
 			return strResult;
 		}
 
+
+        /// <summary>
+        /// Take XML response from BroadView, load it into an XML document.
+        /// Load up the appropriate XSLT into an XML document to transform Broadview XML into Dataset XML.
+        /// Transform the source XML into DataSet XML.
+        /// Load the Dataset XML into a Dataset and return it to the caller.
+        /// </summary>
+        /// <param name="xmlString"></param>
+        /// <param name="xsltFile"></param>
+        /// <returns></returns>
 		public static DataSet ToDataSet(string xmlString, string xsltFile)
 		{
-			//
-			// Trace the input data
-			//
 			Tracer oTracer = new Tracer();
-			oTracer.LogInfo("Entering SchemaConvert.ToDataSet.\nxmlString =\n" + xmlString);
-
-			//
-			// Load and transform the given XML
-			//
-			XmlDocument oXmlDocument = new XmlDocument();
-
+            if (oTracer.IsDebugEnabled)	
+            {
+                oTracer.LogDebug(string.Format("In SchemaConverter.ToDataSet().\nSource XML String=\n{0}\nSource XSLT=\n{1}\n", xmlString, xsltFile));
+            }
+            
+            XmlDocument oXmlDocument = new XmlDocument();
 			try
 			{
 				oXmlDocument.LoadXml(xmlString);
@@ -147,8 +168,8 @@ namespace BVWebService
             XslCompiledTransform oXsl = new XslCompiledTransform();
 			try
 			{
+                oTracer.LogDebug(string.Format("In SchemaConverter.ToDataSet().Loading Source XSLT=\n{0}\n", xsltFile));
                 oXsl.Load(xsltFile);
-                //oXsl.Load(xsltFile, XsltSettings.TrustedXslt, new XmlUrlResolver());
 			}
 			catch(Exception ex)
 			{
@@ -158,71 +179,75 @@ namespace BVWebService
 					ex);
 			}
 
-//			XmlReader oXmlReader = null;
-//			try
-//			{
-//				oXmlReader = oXsl.Transform(oXmlDocument, (XsltArgumentList) null, (XmlResolver) null);
-//			}
-//			catch(Exception ex)
-//			{
-//				throw new ServiceException(
-//					ServiceException.ExceptionCode.XsltTransformationError,
-//					"WebService has run into a problem transforming the incoming XML stream.",
-//					"Input: " + xmlString,
-//					ex);
-//			}
-
 			MemoryStream oMemoryStream = new MemoryStream();
-			oMemoryStream.Position = 0;
-			oXsl.Transform(oXmlDocument, (XsltArgumentList) null, oMemoryStream);
-			oMemoryStream.Flush();
+			//oMemoryStream.Position = 0;
+			//oXsl.Transform(oXmlDocument, (XsltArgumentList) null, oMemoryStream);
+			//oMemoryStream.Flush();
+
+			try
+			{
+                oTracer.LogDebug("In SchemaConverter.ToDataSet().Transforming XML.");
+                oMemoryStream.Position = 0;
+                oXsl.Transform(oXmlDocument, (XsltArgumentList)null, oMemoryStream);
+                oMemoryStream.Flush();
+			}
+			catch(Exception ex)
+			{
+				throw new ServiceException(
+					ServiceException.ExceptionCode.XsltTransformationError,
+					"WebService has run into a problem transforming the incoming XML to DataSet.",
+					"Input: " + oXmlDocument.OuterXml,
+					ex);
+			}
+
+            DataSet oDataSet = new DataSet();
 
 			//
 			// Take a look at the transformation result!
 			//
-			StreamReader oStreamReader = new StreamReader(oMemoryStream);
-            if (oTracer.IsDebugEnabled)
-			{
-				oMemoryStream.Position = 0;
-				oTracer.LogInfo("In SchemaConverter.ToDataSet.\nTransformed result=\n" + oStreamReader.ReadToEnd());
-			}
+            using (StreamReader oStreamReader = new StreamReader(oMemoryStream))
+            {
+                if (oTracer.IsDebugEnabled)
+                {
+                    oMemoryStream.Position = 0;
+                    oTracer.LogDebug(string.Format("In SchemaConverter.ToDataSet().\nTransformed XML Result=\n{0}\n", oStreamReader.ReadToEnd()));
+                }
 
-			//
-			// Load the result back into a dataset
-			//
-			DataSet oDataSet = new DataSet();
 
-			try
-			{
-				oMemoryStream.Position = 0;
-                oDataSet.ReadXml(oMemoryStream);
+                //
+                // Load the result back into a dataset
+                //
+                try
+                {
+                    oMemoryStream.Position = 0;
+                    oDataSet.ReadXml(oMemoryStream);
+                }
+                catch (Exception ex)
+                {
+                    oMemoryStream.Position = 0;
+                    throw new ServiceException(
+                        ServiceException.ExceptionCode.InvalidDataSet,
+                        "WebService has run into a problem converting xslt output into a dataset.",
+                        "Input: " + oStreamReader.ReadToEnd(),
+                        ex);
+                }
+            }
+           
 
-			}
-			catch(Exception ex)
-			{
-				oMemoryStream.Position = 0;
-				throw new ServiceException(
-					ServiceException.ExceptionCode.InvalidDataSet,
-					"WebService has run into a problem converting xslt output into a dataset.",
-					"Input: " + oStreamReader.ReadToEnd(),
-					ex);
-			}
-			//oXmlReader.Close();
-			oStreamReader.Close();
+			
 
-			//
-			// Trace the DataSet if needed
-			//
+
             if (oTracer.IsDebugEnabled)	// Check the switch before tracing (we don't want to extract the XML if the switch is OFF)...
 			{
 				using (StringWriter oStringWriter = new StringWriter())
 				{
 					oDataSet.WriteXml(oStringWriter, XmlWriteMode.WriteSchema | XmlWriteMode.DiffGram);
-					oTracer.LogInfo("Leaving SchemaConverter.ToDataSet.\nDataSet= \n" + oStringWriter.ToString());
+                    oTracer.LogDebug(string.Format("In SchemaConverter.ToDataSet().\nResult Dataset DiffGram=\n{0}\n", oStringWriter.ToString()));
 				}
+
 			}
 
-			string sXML = oDataSet.GetXml();
+			//string sXML = oDataSet.GetXml();
 
 			return oDataSet;
 		}
